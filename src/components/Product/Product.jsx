85840339af3bb51685661556f430d69d8735c093
Temiloluwa <temiloluwaOgunti8@gmail.com>
2024-11-27 19:18:00 +0100

    Designing with tailwind

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,25 +1,28 @@
-import { ShoppingCart } from 'lucide-react';
-import { Link } from 'react-router-dom';
-import { commaSeparatedPrice } from '@/utils/helperFunctions';
-import PropTypes from 'prop-types';
+import { ShoppingCart } from "lucide-react";
+import { Link } from "react-router-dom";
+import { commaSeparatedPrice } from "@/utils/helperFunctions";
+import PropTypes from "prop-types";
 
-const Product = ({product}) => {
+const Product = ({ product }) => {
   return (
-    <div className="group relative">
-        <Link to={`/product/${product._id}`}> <img
-        alt={product.name}
-        src={product.image}
-        className="aspect-square w-full rounded-md cursor-pointer bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
-        loading="lazy"
-      />
-        </Link>
-     
+    <div className="group relative w-40">
+      <Link to={`/product/${product._id}`}>
+        <img
+          alt={product.name}
+          src={product.image}
+          className="aspect-square w-full rounded-md cursor-pointer bg-gray-200 object-cover"
+          loading="lazy"
+        />
+      </Link>
+
       <div className="mt-4 flex justify-between">
         <div>
           <h3 className="text-sm text-gray-700">
-            <div className='capitalize'>{product.name}</div>
+            <div className="capitalize">{product.name}</div>
           </h3>
-          <p className="text-sm font-medium text-gray-900">{commaSeparatedPrice(product.price)}</p>
+          <p className="text-sm font-medium text-gray-900">
+            {commaSeparatedPrice(product.price)}
+          </p>
         </div>
         <p className="text-sm font-medium text-gray-900 cursor-pointer">
           <ShoppingCart color="orange" />
@@ -27,7 +30,7 @@ const Product = ({product}) => {
       </div>
     </div>
   );
-}
+};
 Product.propTypes = {
   product: PropTypes.shape({
     name: PropTypes.string.isRequired,
--- src/components/product-page/ProductPage.jsx
+++ src/components/product-page/ProductPage.jsx
@@ -1,16 +1,25 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import { commaSeparatedPrice } from "@/utils/helperFunctions";
+import ProductList from "../product-list/ProductList";
+
+import { Button } from "../ui/button";
+
 import axiosService from "@/axios";
 
 const ProductPage = () => {
   const { productId } = useParams();
-  const {data: product, isLoading, error} = useQuery({
+  const {
+    data: product,
+    isLoading,
+    error,
+  } = useQuery({
     queryKey: ["product", productId],
-    queryFn: async() => {
+    queryFn: async () => {
       const response = await axiosService.get(`/products/${productId}`);
       return response.data;
-    }
-  })
+    },
+  });
   if (isLoading) {
     return (
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
@@ -20,28 +29,65 @@ const ProductPage = () => {
       </div>
     );
   }
+
+  if (error) {
+    return <div>There was an error fetching product: {error.message}</div>;
+  }
+
   return (
-    <div className="grid grid-cols-2">
-      <img src={product.image} alt={product.name} loading="lazy" />
-      <section>
-        <div>
-          <h4>{product.name}</h4>
-          <div>
-            <span>Category: {product.category}</span>
-            <span>Brand: {product.brand}</span>
+    <>
+      <div className="grid grid-cols-2 p-2 gap-4 mb-10">
+        <img
+          src={product.image}
+          alt={product.name}
+          loading="lazy"
+          className="aspect-square w-full rounded-md cursor-pointer bg-gray-200 object-cover object-center group-hover:opacity-75 lg:aspect-auto lg:h-96"
+        />
+        <section>
+          <div className="my-4">
+            <h4 className="text-3xl capitalize">{product.name}</h4>
+            <div className="text-xs">
+              <span>Category: {product.category.name}</span>
+              <span>Brand: {product.brand}</span>
+            </div>
           </div>
-        </div>
-        <div>
-          Price: {product.price}
-        </div>
-        <div>
-          {
-            product.description
-          }
-        </div>
-        
-      </section>
-    </div>
+          <div className="font-semibold text-3xl my-4">
+            &#8358;
+            {commaSeparatedPrice(product.price)}
+          </div>
+          <div className="mt-2">{product.description}</div>
+          <div className="flex gap-4 my-4">
+            <div>
+              <Button
+                className="rounded-tr-none rounded-br-none border-black"
+                variant="outline"
+              >
+                -
+              </Button>
+              <Button
+                className="rounded-none border border-black"
+                variant="outline"
+              >
+                0
+              </Button>
+              <Button
+                className="rounded-tl-none rounded-bl-none border border-black"
+                variant="outline"
+              >
+                +
+              </Button>
+            </div>
+            <Button>Add to cart</Button>
+          </div>
+          <p className="">*Delivery only within lagos</p>
+        </section>
+      </div>
+      <ProductList
+        title={"Similar products"}
+        categoryId={product.category._id}
+        slug={product.category.name}
+      />
+    </>
   );
 };
 
