f639ead2f3e870786a124865f0f5eb79f1dc693d
Temiloluwa <temiloluwaOgunti8@gmail.com>
2024-12-12 22:17:43 +0100

    Updated axios route

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,7 @@ import axios from "axios";
 import { toast } from "sonner";
 
 const axiosService = axios.create({
-  // baseURL: "https://double-decent-server.onrender.com/api/v1/",
-  baseURL: "http://localhost:5000/api/v1/",
+  baseURL: "https://double-decent-server.onrender.com/api/v1/",
   headers: { "Content-Type": "application/json" },
 });
 
